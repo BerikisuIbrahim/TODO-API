a6da252fed7fc56111289a118a8c321b39b36be9
Berikisu Ibnrahim <berikisuibrahim227@gmail.com>
2024-10-16 15:28:37 +0000

    installed bcryptjs, express-jwt, jsonwebtoken and made some changes

--- models/todo_models.js
+++ models/todo_models.js
@@ -1,10 +1,15 @@
 //import { required } from "joi";
+import { toJSON } from "@reis/mongoose-to-json";
 import { Schema, model } from "mongoose";
 
 const todoSchema = new Schema({
     title: {type: String, required: true},
     icon: {type: String, required: true },
     completed: {type: Boolean, default: false}
+}, {
+    timestamps: true
 });
+
+todoSchema.plugin(toJSON);
 
 export const TodoModel = model('Todo', todoSchema);
--- models/user_models.js
+++ models/user_models.js
@@ -1,11 +1,16 @@
-import { type } from "express/lib/response";
+//import { type } from "express/lib/response";
 // import { required } from "joi";
 import { Schema, model } from "mongoose";
+import { toJSON } from "@reis/mongoose-to-json"
 
 const userSchema = new Schema({
-   name: {type:String, required: true},
-    email: {type:String, required:true, unique:true},
-   password: {type:String, required:true}
+   name: { type: String, required: true },
+   email: { type: String, required: true, unique: true },
+   password: { type: String, required: true }
+}, {
+   timestamps: true
 });
 
-//export const UserModel = model{"User", userSchema}
+userSchema.plugin(toJSON);
+
+export const UserModel = model("User", userSchema);
